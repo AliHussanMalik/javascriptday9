1c4eb1b47831752e9f7e2d527bf38f84a5a319b8
Ali Hussan Malik <malikalihussan2@gmail.com>
2024-08-09 19:43:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// console.log("Appsgenii");
+
+// let variable2 = "abc";
+// let variable4 = "abc".toUpperCase();
+// let variable3 = [1, 2, 3];
+// console.log(variable2);
+// console.log(variable3);
+// let variable5 = [1, 2, 3].push(4);
+
+// console.log(variable3);
+// console.log(variable4);
+// console.log(variable5);
+
+// function sum(a, b) {
+//   sum.log(a + b);
+// }
+// return sum;
+
+// function message(msg, beloved) {
+//   //parameter
+//   console.log(msg, beloved);
+// }
+// message("I Love You", "Somebody"); //argument passed
+
+function person() {
+  this.greet = "Hello";
+}
+console.log(this.greet);
